a41ea0f133e5edc78d0740060cb0736d4d51761e
headset <headset@devheadset.org>
2024-10-19 19:18:27 -0400

    removing neo tree, adding nvim-tree
    adding nvim-tree, removing neo-tree, also removing a uneeded line in
    themery.lua

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -3,7 +3,7 @@ return
     "zaldih/themery.nvim",
     config = function()
       require("themery").setup({
-        themes = {"gruvbox", "ayu", "tokyonight"}, -- Your installed themes
+    
         livePreview = true, -- Enable live preview
       })
     end
